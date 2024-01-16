313b9a41a4c312cd00864c220392a2b9ed1d0ec2
emanuela <emanuela.markovic@hotmail.com>
2024-01-16 15:49:53 +0100

    fixes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
   // set base to repo for deploy on gh-pages
-  base: "/react_repo/",
+  base: "/react_-repo/",
   plugins: [react()],
   build: {
     outDir: "build",
